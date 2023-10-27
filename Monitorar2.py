c452eb0a670355688fbfc51083ffd066696948b5
Matheus Dorigo <77252441+matheusdorigo@users.noreply.github.com>
2023-10-27 05:10:42 -0300

    Update Monitorar2.py

--- Monitorar2.py
+++ Monitorar2.py
@@ -19,10 +19,10 @@ options.add_argument("window-size=300,300")
  # Configura o driver do Edge
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()),options=options)
 # Abre a URL
-url = "https://tickets.sinch.com/projects/APPS/queues/custom/19"  # Substitua por sua URL
+url = "URL"  # Substitua por sua URL
 driver.get(url)
 time.sleep(3)
-## Acessar a pagina inicial e clicar em "Sinch Employees"
+## Acessar a pagina inicial e clicar em "HREF"
 acessar = driver.find_elements(By.XPATH, '/html/body/div/div/section/div/div/section/div/p[1]/a')
 for i in acessar:
     print("\n")
@@ -39,64 +39,62 @@ print("Lista das filas monitoradas:\n")
 elements = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[43]/a/aui-badge')
 for e in elements:
     if int(e.text) != 0:
-        ##print("***** Ticket Aberto ***** em [24x7] - On going: "+e.text+"\n")
-        toast = Notification(app_id="[24x7] - On going",title="Atenção",msg="[24x7] - On going: "+e.text,duration="long")
+        ##print("***** Ticket Aberto ***** em TEXTO - On going: "+e.text+"\n")
+        toast = Notification(app_id="TEXTO - On going",title="Atenção",msg="TEXTO - On going: "+e.text,duration="long")
         toast.set_audio(audio.Mail, loop=True)
-        toast.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/2443")
+        toast.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/2443")
         toast.show()
 else:
-       print("[24x7] - On going: "+e.text+"\n")
+       print("TEXTO - On going: "+e.text+"\n")
 element4 = driver.find_elements(By.XPATH, '//html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[42]/a/aui-badge')
 for t in element4:   
   if int(t.text) != 0:
-    toast3 = Notification(app_id="[24x7] News - Triagem",title="Atenção",msg="[24x7] News - Triagem: "+t.text,duration="long")
+    toast3 = Notification(app_id="TEXTO TEXTO - Triagem",title="Atenção",msg="TEXTO TEXTO - Triagem: "+t.text,duration="long")
     toast3.set_audio(audio.Mail, loop=True)
-    toast3.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/2442")
+    toast3.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/2442")
     toast3.show()
 else: 
-       print("[24x7] News - Triagem: "+t.text+"\n")
+       print("TEXTO TEXTO - Triagem: "+t.text+"\n")
 element1 = driver.find_elements(By.XPATH, '//html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[36]/a/aui-badge')
 for m in element1:
     if int(m.text) != 0:
-        ##print("*** Ticket aberto *** em [Enter] News - Triagem: "+m.text+"\n")
-     toast4 = Notification(app_id="[Enter] News - Triagem",title="Atenção",msg="[Enter] News - Triagem: "+t.text,duration="long") 
+        ##print("*** Ticket aberto *** em [Enter] TEXTO - Triagem: "+m.text+"\n")
+     toast4 = Notification(app_id="[Enter] TEXTO - Triagem",title="Atenção",msg="[Enter] TEXTO - Triagem: "+t.text,duration="long") 
      toast4.set_audio(audio.Mail, loop=True)
-     toast4.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/2209")
+     toast4.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/2209")
      toast4.show()
 else:
-        print("[Enter] News - Triagem: "+m.text+"\n")
+        print("[Enter] TEXTO - Triagem: "+m.text+"\n")
 element2 = driver.find_elements(By.XPATH, '//html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[20]/a/aui-badge')
 
 
 for n in element2:
     if int (n.text) != 0:
-        ##print("***** Ticket aberto ***** em [Apps] News - Screening: "+n.text+"\n")
-         toast5 = Notification(app_id="[Apps] News - Screening",title="Atenção",msg="[Apps] News - Screening: "+n.text,duration="long")
+        ##print("***** Ticket aberto ***** em [Apps] TEXTO - TEXTO: "+n.text+"\n")
+         toast5 = Notification(app_id="[Apps] TEXTO - TEXTO",title="Atenção",msg="[Apps] TEXTO - TEXTO: "+n.text,duration="long")
          toast5.set_audio(audio.Mail, loop=True)
-         toast5.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/2208")
+         toast5.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/2208")
          toast5.show()
 else:
-    print("[Apps] News - Screening: "+n.text+"\n")
+    print("[Apps] TEXTO - TEXTO: "+n.text+"\n")
     element3 = driver.find_elements(By.XPATH, '//html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[2]/a/aui-badge')
 for u in element3:
     if int (u.text) != 2:
-      toast1 = Notification(app_id="Uncategorized News - Triagem",title="Atenção",msg="Uncategorized News - Triagem: "+u.text,duration="long")
+      toast1 = Notification(app_id="Uncategorized TEXTO - Triagem",title="Atenção",msg="Uncategorized TEXTO - Triagem: "+u.text,duration="long")
       toast1.set_audio(audio.Mail, loop=True)
-      toast1.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/18")
+      toast1.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/18")
       toast1.show()
 else:
-     print("Uncategorized News - Triagem: "+u.text+"\n")
+     print("Uncategorized TEXTO - Triagem: "+u.text+"\n")
 element5 = driver.find_elements(By.XPATH, '//html/body/div[1]/div/div/section/div[1]/div[3]/div/div/ul/li[4]/a/aui-badge')
 for t in element5:
     if int(t.text) != 5:
       toast2 = Notification(app_id="Abastible_apps",title="Atenção",msg="Abastible_apps: "+t.text,duration="long")
       toast2.set_audio(audio.Mail, loop=True)
-      toast2.add_actions(label="Acessar fila",launch="https://tickets.sinch.com/projects/APPS/queues/custom/2286")
+      toast2.add_actions(label="Acessar fila",launch="TEXTOqueues/custom/2286")
       toast2.show()
 else:
           print("Abastible_apps: "+t.text+"\n") 
 
           print(" *** Encerrando consulta *** \n")
            
-
-
